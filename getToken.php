52237d6e2e69eafab1408b9069649ae6b150e9bf
raulgarafulic <rgarafulicm@gmail.com>
2018-03-22 08:54:09 -0400

    17

--- getToken.php
+++ getToken.php
@@ -1,5 +1,5 @@
 <?php
-  header("Access-Control-Allow-Origin: https://p7vip-9d6eb.firebaseapp.com:443");
+  header("Access-Control-Allow-Origin: *");
   header("Access-Control-Allow-Methods: POST");
   header("Access-Control-Allow-Headers: Content-Type, Authorization");
   header("Content-Type: application/json");
