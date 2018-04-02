fc3d2261717412b089499e7debf272660f8434e0
raulgarafulic <rgarafulicm@gmail.com>
2018-04-02 17:54:03 -0400

    20

--- crearpagos.php
+++ crearpagos.php
@@ -4,38 +4,38 @@
   $contact = json_decode(file_get_contents("php://input"));
   $receiver_id = 162163;
   $secret = '2bd49ecf95c4375dc3e09e89d98cf284d9420939';
-
-  require __DIR__ . '/vendor/autoload.php';
-
-  $configuration = new Khipu\Configuration();
-  $configuration->setReceiverId($receiver_id);
-  $configuration->setSecret($secret);
-
-  $client = new Khipu\ApiClient($configuration);
-  $payments = new Khipu\Client\PaymentsApi($client);
-
-  try {
-    $opts = array (
-      "transaction_id" => "MTI-100",
-      "return_url" => "https://p7vip-9d6eb.firebaseapp.com/comprobante",
-      "cancel_url" => "http://www.paginasiete.bo",
-      "picture_url" => "http://mi-ecomerce.com/pictures/foto-producto.jpg",
-      "notify_url" => "https://p7vip.xyz/recibirpago.php/",
-      "notify_api_version" => "1.3",
-      "payer_email" => $contact->mail
-    );
-    $response = $payments->paymentsPost(
-      "Suscripcion del paquete " . $contact->prod, //Motivo de la compra
-      "BOB", //Moneda
-      $contact->price, //Monto
-      $opts //campos opcionales
-    );
-    // $url = "Location: " . $response->getPaymentUrl();
-    $url = $response->getPaymentUrl();
-    echo $url;
-    // header($url, true, 301);
+  print_r ($contact);
+  // require __DIR__ . '/vendor/autoload.php';
   //
-  } catch (\Khipu\ApiException $e) {
-      echo print_r($e->getResponseBody(), TRUE);
-  }
+  // $configuration = new Khipu\Configuration();
+  // $configuration->setReceiverId($receiver_id);
+  // $configuration->setSecret($secret);
+  //
+  // $client = new Khipu\ApiClient($configuration);
+  // $payments = new Khipu\Client\PaymentsApi($client);
+  //
+  // try {
+  //   $opts = array (
+  //     "transaction_id" => "MTI-100",
+  //     "return_url" => "https://p7vip-9d6eb.firebaseapp.com/comprobante",
+  //     "cancel_url" => "http://www.paginasiete.bo",
+  //     "picture_url" => "http://mi-ecomerce.com/pictures/foto-producto.jpg",
+  //     "notify_url" => "https://p7vip.xyz/recibirpago.php/",
+  //     "notify_api_version" => "1.3",
+  //     "payer_email" => $contact->mail
+  //   );
+  //   $response = $payments->paymentsPost(
+  //     "Suscripcion del paquete " . $contact->prod, //Motivo de la compra
+  //     "BOB", //Moneda
+  //     $contact->price, //Monto
+  //     $opts //campos opcionales
+  //   );
+  //   // $url = "Location: " . $response->getPaymentUrl();
+  //   $url = $response->getPaymentUrl();
+  //   echo $url;
+  //   // header($url, true, 301);
+  // //
+  // } catch (\Khipu\ApiException $e) {
+  //     echo print_r($e->getResponseBody(), TRUE);
+  // }
 ?>
