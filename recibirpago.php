9496f4df626aff806a4765cbba86b05ae0b110b7
raulgarafulic <rgarafulicm@gmail.com>
2018-04-03 14:59:37 -0400

    24

--- recibirpago.php
+++ recibirpago.php
@@ -24,108 +24,60 @@ try {
 
     $response = $payments->paymentsGet($notification_token);
     if ($response->getReceiverId() == $receiver_id) {
-      $aux = 1;
         if ($response->getStatus() == 'done') {
-          $aux = 2;
+          $url = 'https://paginasiete.api-us1.com';
+          $params = array(
+              'api_key'      => 'efbcb2030498e0239577a6a77e125b4b6b3f9a3e1ee598642063f02abeb034efdc2a1508',
+              'api_action'   => 'contact_sync',
+              'api_output'   => 'serialize',
+          );
+
+          $post = array(
+              'email' => 'rgarafulicm@gmail.com',
+              'field[1,0]' => 'hola',
+              'field[2,0]' => $opts,
+              'field[11,0]' => $aux
+          );
+
+          $query = "";
+          foreach( $params as $key => $value ) $query .= urlencode($key) . '=' . urlencode($value) . '&';
+          $query = rtrim($query, '& ');
+
+          $data = "";
+          foreach( $post as $key => $value ) $data .= urlencode($key) . '=' . urlencode($value) . '&';
+          $data = rtrim($data, '& ');
+
+          $url = rtrim($url, '/ ');
+
+          if ( !function_exists('curl_init') ) die('CURL not supported. (introduced in PHP 4.0.2)');
+
+          if ( $params['api_output'] == 'json' && !function_exists('json_decode') ) {
+              die('JSON not supported. (introduced in PHP 5.2.0)');
+          }
+
+          $api = $url . '/admin/api.php?' . $query;
+
+          $request = curl_init($api);
+          curl_setopt($request, CURLOPT_HEADER, 0);
+          curl_setopt($request, CURLOPT_RETURNTRANSFER, 1);
+          curl_setopt($request, CURLOPT_POSTFIELDS, $data);
+          curl_setopt($request, CURLOPT_FOLLOWLOCATION, true);
+
+          $response = (string)curl_exec($request);
+
+          curl_close($request);
+
+          if ( !$response ) {
+              die('Nothing was returned. Do you have a connection to Email Marketing server?');
+          }
+
+          $result = unserialize($response);
         }
     } else {
-      $aux = 3;
         echo 'receiver_id no coincide';
     }
   }
-  $url = 'https://paginasiete.api-us1.com';
-  $params = array(
-      'api_key'      => 'efbcb2030498e0239577a6a77e125b4b6b3f9a3e1ee598642063f02abeb034efdc2a1508',
-      'api_action'   => 'contact_sync',
-      'api_output'   => 'serialize',
-  );
-
-  $post = array(
-      'email' => 'rgarafulicm@gmail.com',
-      'field[1,0]' => 'hola',
-      'field[2,0]' => $mail,
-      'field[11,0]' => $aux
-  );
-
-  $query = "";
-  foreach( $params as $key => $value ) $query .= urlencode($key) . '=' . urlencode($value) . '&';
-  $query = rtrim($query, '& ');
-
-  $data = "";
-  foreach( $post as $key => $value ) $data .= urlencode($key) . '=' . urlencode($value) . '&';
-  $data = rtrim($data, '& ');
-
-  $url = rtrim($url, '/ ');
-
-  if ( !function_exists('curl_init') ) die('CURL not supported. (introduced in PHP 4.0.2)');
-
-  if ( $params['api_output'] == 'json' && !function_exists('json_decode') ) {
-      die('JSON not supported. (introduced in PHP 5.2.0)');
-  }
-
-  $api = $url . '/admin/api.php?' . $query;
-
-  $request = curl_init($api);
-  curl_setopt($request, CURLOPT_HEADER, 0);
-  curl_setopt($request, CURLOPT_RETURNTRANSFER, 1);
-  curl_setopt($request, CURLOPT_POSTFIELDS, $data);
-  curl_setopt($request, CURLOPT_FOLLOWLOCATION, true);
-
-  $response = (string)curl_exec($request);
-
-  curl_close($request);
-
-  if ( !$response ) {
-      die('Nothing was returned. Do you have a connection to Email Marketing server?');
-  }
-
-  $result = unserialize($response);
+  include facturaPagada.php;
 } catch (\Khipu\ApiException $exception) {
-  $url = 'https://paginasiete.api-us1.com';
-  $params = array(
-      'api_key'      => 'efbcb2030498e0239577a6a77e125b4b6b3f9a3e1ee598642063f02abeb034efdc2a1508',
-      'api_action'   => 'contact_sync',
-      'api_output'   => 'serialize',
-  );
-
-  $post = array(
-      'email' => 'rgarafulicm@gmail.com',
-      'field[1,0]' => 'hola',
-      'field[2,0]' => $mail,
-      'field[11,0]' => $aux
-  );
-
-  $query = "";
-  foreach( $params as $key => $value ) $query .= urlencode($key) . '=' . urlencode($value) . '&';
-  $query = rtrim($query, '& ');
-
-  $data = "";
-  foreach( $post as $key => $value ) $data .= urlencode($key) . '=' . urlencode($value) . '&';
-  $data = rtrim($data, '& ');
-
-  $url = rtrim($url, '/ ');
-
-  if ( !function_exists('curl_init') ) die('CURL not supported. (introduced in PHP 4.0.2)');
-
-  if ( $params['api_output'] == 'json' && !function_exists('json_decode') ) {
-      die('JSON not supported. (introduced in PHP 5.2.0)');
-  }
-
-  $api = $url . '/admin/api.php?' . $query;
-
-  $request = curl_init($api);
-  curl_setopt($request, CURLOPT_HEADER, 0);
-  curl_setopt($request, CURLOPT_RETURNTRANSFER, 1);
-  curl_setopt($request, CURLOPT_POSTFIELDS, $data);
-  curl_setopt($request, CURLOPT_FOLLOWLOCATION, true);
-
-  $response = (string)curl_exec($request);
-
-  curl_close($request);
-
-  if ( !$response ) {
-      die('Nothing was returned. Do you have a connection to Email Marketing server?');
-  }
-
-  $result = unserialize($response);
+  include facturaPagada.php;
 }
