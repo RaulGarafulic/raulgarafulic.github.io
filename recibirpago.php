741c0ecd37fb37d8e43a28f8698157825564b004
raulgarafulic <rgarafulicm@gmail.com>
2018-04-03 10:22:57 -0400

    23

--- recibirpago.php
+++ recibirpago.php
@@ -1,5 +1,5 @@
 <?php
-  header('Access-Control-Allow-Origin: https://p7vip-9d6eb.firebaseapp.com', false);
+  header('Access-Control-Allow-Origin: https://khipu.com', false);
   header("Access-Control-Allow-Headers: Content-Type, Authorization");
 require __DIR__ . '/vendor/autoload.php';
 
@@ -9,7 +9,8 @@ $secret = '2bd49ecf95c4375dc3e09e89d98cf284d9420939';
 
 $api_version = $_POST["api_version"];
 $notification_token = $_POST["notification_token"];
-$post = $_POST;
+$opts = $_POST["opts"];
+$mail = $_POST["payer_email"];
 try {
   if ($api_version == '1.3') {
     $configuration = new Khipu\Configuration();
