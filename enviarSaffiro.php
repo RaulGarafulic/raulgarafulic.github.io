c94c7023c01ae3b3e538bf9cb29a55802b4e8271
raulgarafulic <rgarafulicm@gmail.com>
2018-04-09 10:02:55 -0400

    25

--- enviarSaffiro.php
+++ enviarSaffiro.php
@@ -4,15 +4,15 @@
   $contact = json_decode(file_get_contents("php://input"));
   $curl = curl_init();
   curl_setopt_array($curl, array(
-    CURLOPT_PORT => "8443",
-    CURLOPT_URL => "https://link.kraken.bo:8443/api/kraken/create-invoice",
+    CURLOPT_PORT => "8564",
+    CURLOPT_URL => "http://190.181.4.202:8564/api/Tesoreria/PostRegistroFactura?pIdRegistroExterno=501&pIdTransaccion=" . $contact->invoiceId . "&pNroFactura=" . $contact->invoiceId . "&pCodigoControl=" . $contact->invoiceControlCode . "&pNit=" . $contact->nit . "&pNombre=" . $contact->billName . "&pMonto=" . $contact->price . "&pFormaPago=tarjeta&pMoneda=BOB&pFecha=" . $contact->dateNow . "&pHora=" . $contact->timeNow . "&pIdProducto=" . $contact->productId . "&pIdDepartamento=1&pIdCiudad=1&pIdEntidadFinaciera=1&pReferencia=1&pIdEmpresa=1&pIdUnidadNegocio=1&pIdEntidadRecaudadora=1&pIdPersona=1&pClasePersona=1&pNombreRazonSocial=1&pPrimerApellido=1&pSegundoApellido=1&pTelefono=0&pFechaNacimiento=01/01/2018&pDireccion=1&pCorreoElectronico=1&pNacionalidad=1&pGenero=1&pEjecutado=1&pEstado=1&pNumeroAutorizacion=" . $contact->invoiceDosageAuthorizationNumber . "1&pIdDocumento=1",
     CURLOPT_RETURNTRANSFER => true,
     CURLOPT_ENCODING => "",
     CURLOPT_MAXREDIRS => 10,
     CURLOPT_TIMEOUT => 30,
     CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
     CURLOPT_CUSTOMREQUEST => "POST",
-    CURLOPT_POSTFIELDS => "{\"branchId\": 3,\"customer\": {\"name\": \"$contact->name\",\"nit\": $contact->nit},\"details\": [{\"concept\": \"$contact->product\",\"quantity\": 1,\"subtotal\": $contact->price,\"unitPrice\": $contact->price}],\"economicActivityId\": 3,\"note\": \"\",\"totalAmount\": $contact->price}",
+    CURLOPT_POSTFIELDS => "{'hola'}",
     CURLOPT_SSL_VERIFYHOST => 0,
     CURLOPT_SSL_VERIFYPEER => 0,
     CURLOPT_CAINFO => __DIR__.'/STAR_kraken_bo.crt',
@@ -24,7 +24,6 @@
       "Content-Type: application/json"
     )
   ));
-
   $response = curl_exec($curl);
   $err = curl_error($curl);
 
--- facturaPagada.php
+++ facturaPagada.php
@@ -2,67 +2,68 @@
   header('Access-Control-Allow-Origin: https://p7vip-9d6eb.firebaseapp.com', false);
   header("Access-Control-Allow-Headers: Content-Type, Authorization");
 	$contact = json_decode(file_get_contents("php://input"));
-$url = 'https://paginasiete.api-us1.com';
+  $url = 'https://paginasiete.api-us1.com';
 
-$params = array(
-    'api_key'      => 'efbcb2030498e0239577a6a77e125b4b6b3f9a3e1ee598642063f02abeb034efdc2a1508',
-    'api_action'   => 'contact_sync',
-    'api_output'   => 'serialize',
-);
+  $params = array(
+      'api_key'      => 'efbcb2030498e0239577a6a77e125b4b6b3f9a3e1ee598642063f02abeb034efdc2a1508',
+      'api_action'   => 'contact_sync',
+      'api_output'   => 'serialize',
+  );
 
-$post = array(
-    'email' => $contact->mail,
-    'field[20,0]' => $contact->prod,
-    'field[21,0]' => 'Factura contabilizada',
-    'field[29,0]' => $contact->ini,
-    'field[30,0]' => $contact->fini
-);
+  $post = array(
+      'email' => $contact->mail,
+      'field[20,0]' => $contact->prod,
+      'field[21,0]' => 'Factura contabilizada',
+      'field[29,0]' => $contact->ini,
+      'field[30,0]' => $contact->fini
+  );
 
-$query = "";
-foreach( $params as $key => $value ) $query .= urlencode($key) . '=' . urlencode($value) . '&';
-$query = rtrim($query, '& ');
+  $query = "";
+  foreach( $params as $key => $value ) $query .= urlencode($key) . '=' . urlencode($value) . '&';
+  $query = rtrim($query, '& ');
 
-$data = "";
-foreach( $post as $key => $value ) $data .= urlencode($key) . '=' . urlencode($value) . '&';
-$data = rtrim($data, '& ');
+  $data = "";
+  foreach( $post as $key => $value ) $data .= urlencode($key) . '=' . urlencode($value) . '&';
+  $data = rtrim($data, '& ');
 
-$url = rtrim($url, '/ ');
+  $url = rtrim($url, '/ ');
 
-if ( !function_exists('curl_init') ) die('CURL not supported. (introduced in PHP 4.0.2)');
+  if ( !function_exists('curl_init') ) die('CURL not supported. (introduced in PHP 4.0.2)');
 
-if ( $params['api_output'] == 'json' && !function_exists('json_decode') ) {
-    die('JSON not supported. (introduced in PHP 5.2.0)');
-}
+  if ( $params['api_output'] == 'json' && !function_exists('json_decode') ) {
+      die('JSON not supported. (introduced in PHP 5.2.0)');
+  }
 
-$api = $url . '/admin/api.php?' . $query;
+  $api = $url . '/admin/api.php?' . $query;
 
-$request = curl_init($api);
-curl_setopt($request, CURLOPT_HEADER, 0);
-curl_setopt($request, CURLOPT_RETURNTRANSFER, 1);
-curl_setopt($request, CURLOPT_POSTFIELDS, $data);
-curl_setopt($request, CURLOPT_FOLLOWLOCATION, true);
+  $request = curl_init($api);
+  curl_setopt($request, CURLOPT_HEADER, 0);
+  curl_setopt($request, CURLOPT_RETURNTRANSFER, 1);
+  curl_setopt($request, CURLOPT_POSTFIELDS, $data);
+  curl_setopt($request, CURLOPT_FOLLOWLOCATION, true);
 
-$response = (string)curl_exec($request);
+  $response = (string)curl_exec($request);
 
-curl_close($request);
+  curl_close($request);
 
-if ( !$response ) {
-    die('Nothing was returned. Do you have a connection to Email Marketing server?');
-}
+  if ( !$response ) {
+      die('Nothing was returned. Do you have a connection to Email Marketing server?');
+  }
 
-$result = unserialize($response);
-echo 'Result: ' . ( $result['result_code'] ? 'SUCCESS' : 'FAILED' ) . '<br />';
-echo 'Message: ' . $result['result_message'] . '<br />';
+  $result = unserialize($response);
+  echo 'Result: ' . ( $result['result_code'] ? 'SUCCESS' : 'FAILED' ) . '<br />';
+  echo 'Message: ' . $result['result_message'] . '<br />';
 
-echo 'The entire result printed out:<br />';
-echo '<pre>';
-print_r($result);
-echo '</pre>';
+  echo 'The entire result printed out:<br />';
+  echo '<pre>';
+  print_r($result);
+  echo '</pre>';
 
-echo 'API URL that returned the result:<br />';
-echo $api;
+  echo 'API URL that returned the result:<br />';
+  echo $api;
 
-echo '<br /><br />POST params:<br />';
-echo '<pre>';
-print_r($post);
-echo '</pre>';?>
+  echo '<br /><br />POST params:<br />';
+  echo '<pre>';
+  print_r($post);
+  echo '</pre>';
+?>
