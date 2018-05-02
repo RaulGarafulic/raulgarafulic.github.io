20a2503a8ade459727d49e9640ba8ae159352443
raulgarafulic <rgarafulicm@gmail.com>
2018-05-02 09:46:08 -0400

    42

--- crearpagos.php
+++ crearpagos.php
@@ -21,7 +21,8 @@
       "picture_url" => "https://p7vip-9d6eb.firebaseapp.com/assets/P7VIPlogo.jpg",
       "notify_url" => "https://p7vip.xyz/recibirpago.php?mail=".$contact->mail,
       "notify_api_version" => "1.3",
-      "payer_email" => $contact->mail
+      "payer_email" => $contact->mail,
+      "payer_name" => $contact->name
     );
     $response = $payments->paymentsPost(
       "Suscripcion P7 VIP " . $contact->prod . ".", //Motivo de la compra
