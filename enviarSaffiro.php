36a6ad76261ed1b8d3a83578e5b68b6aa5afadf0
raulgarafulic <rgarafulicm@gmail.com>
2018-05-04 14:50:53 -0400

    43

--- enviarSaffiro.php
+++ enviarSaffiro.php
@@ -3,8 +3,17 @@
   header('Access-Control-Allow-Origin: https://p7vip-9d6eb.firebaseapp.com', false);
   header("Access-Control-Allow-Headers: Content-Type");
   $contact = json_decode(file_get_contents("php://input"));
-
-  $url = "http://saffiro2.kantutani.com:8561/api/Tesoreria/PostRegistroFactura?pIdRegistroExterno=1&pIdTransaccion=" . $contact->invoiceId . "&pNroFactura=" . $contact->invoiceNumber . "&pCodigoControl=" . $contact->invoiceControlCode . "&pNit=" . $contact->nit . "&pNombre=" . $contact->billName . "&pMonto=" . $contact->price . "&pFormaPago=" . $contact->formaPago . "&pMoneda=BOL&pFecha=" . $contact->dateNow . "&pHora=" . $contact->timeNow . "&pIdProducto=" . $contact->productId . "&pIdDepartamento=1&pIdCiudad=1&pIdEntidadFinaciera=1&pReferencia=1&pIdEmpresa=2&pIdUnidadNegocio=5&pIdEntidadRecaudadora=1&pIdPersona=1&pClasePersona=1&pNombreRazonSocial=_&pPrimerApellido=_&pSegundoApellido=_&pTelefono=1&pFechaNacimiento=01/01/2017&pDireccion=1&pCorreoElectronico=1&pNacionalidad=1&pGenero=1&pEjecutado=0&pEstado=1&pNumeroAutorizacion=" . $contact->invoiceDosageAuthorizationNumber . "&pIdDocumento=1";
+  // id persona el carnet de la identidad = nit
+  // clase persona todas son naturales = 'nat'
+  // pNombreRazonSocial = nombre completo de la persona.
+  // primer y segundo apellido = _
+  // telefono = telefono
+  // pFechaNacimiento = la fecha del día
+  // direccion = _
+  // correo pCorreoElectronico = añadir
+  // nacionalidad = boliviano
+  // genero = _
+  $url = "http://saffiro2.kantutani.com:8561/api/Tesoreria/PostRegistroFactura?pIdRegistroExterno=1&pIdTransaccion=" . $contact->invoiceId . "&pNroFactura=" . $contact->invoiceNumber . "&pCodigoControl=" . $contact->invoiceControlCode . "&pNit=" . $contact->nit . "&pNombre=" . $contact->billName . "&pMonto=" . $contact->price . "&pFormaPago=" . $contact->formaPago . "&pMoneda=BOL&pFecha=" . $contact->dateNow . "&pHora=" . $contact->timeNow . "&pIdProducto=" . $contact->productId . "&pIdDepartamento=1&pIdCiudad=1&pIdEntidadFinaciera=1&pReferencia=1&pIdEmpresa=2&pIdUnidadNegocio=5&pIdEntidadRecaudadora=1&pIdPersona=" . $contact->nit . "&pClasePersona=nat&pNombreRazonSocial=" . $contact->billName . "&pPrimerApellido=_&pSegundoApellido=_&pTelefono=1&pFechaNacimiento=" . $contact->dateNow . "&pDireccion=_&pCorreoElectronico=" . $contact->mail . "&pNacionalidad=_&pGenero=_&pEjecutado=0&pEstado=1&pNumeroAutorizacion=" . $contact->invoiceDosageAuthorizationNumber . "&pIdDocumento=1";
   echo $url;
   $curl = curl_init();
   curl_setopt_array($curl, array(
